91131e655ba8724ba92d239d036e514155dca410
Mayur Patel <pmayur0680@gmail.com>
2022-04-27 20:00:38 -0500

    added server.js to start server and listen for homepage

---
+++ README.md
@@ -0,0 +1,10 @@
+# Note Taker
+## Description
+Note Taker allows user to write and save notes. This application uses an Express.js back end and will save and retrieve note data from a JSON file. This application is deployed on Heroku.
+## License
+Distributed under the MIT License. See LICENSE.txt for more information.
+## Project Links
+Code Repo: https://github.com/pmayur0680/note-taker<br>
+Live Site: 
+## Screenshots
+## Acknowledgments
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded ( { extended: true }));
+app.use(express.static('public'));
+
+// Route for homepage
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+})
+// listen port
+app.listen(PORT, () => {
+    console.log(`listening at http://localhost:${PORT}`)
+})
